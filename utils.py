cd83d63ba95cf3d1fa20bbbc997559b6cfd75111
Ryan Chan <ryanchankh@berkeley.edu>
2022-10-16 16:35:15 -0400

    update utils

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import os
+import json
+
+
+
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         curr_lr = param_group['lr']
@@ -15,3 +20,10 @@ def get_grad_norm(model):
                 total_norm += param_norm.item() ** 2
     total_norm = total_norm ** 0.5
     return total_norm
+
+
+def save_params(model_dir, params, name='params'):
+    """Save params to a .json file. Params is a dictionary of parameters."""
+    path = os.path.join(model_dir, f'{name}.json')
+    with open(path, 'w') as f:
+        json.dump(params, f, indent=2, sort_keys=True)
