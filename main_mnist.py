ba281056941bab07131a485fdcea966075e3177a
Ryan Chan <kchan49@jhu.edu>
2023-05-25 20:50:04 -0400

    update mnist

--- main_mnist.py
+++ main_mnist.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import torch.optim as optim
